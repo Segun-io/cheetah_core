f4eda5dee3074801611a57341b5a64279835da65
santosa <angel_santos@outlook.com>
2019-04-21 17:27:44 -0500

    adding special fields to items

--- cheetah_core/djangoapps/core/cron.py
+++ cheetah_core/djangoapps/core/cron.py
@@ -19,24 +19,40 @@ def populate_instance(obj, api, data_name):
     data = request.json()
     
     if objs_count == data['result'].get('count', -1):
-        return
+        return False
     
-    data_items = data['result'][data_name]
+    return data['result'][data_name]
 
-    for data_item in data_items:
-        try:
-            obj.objects.get(pk=data_item['id'])
-            continue
-        except obj.DoesNotExist:
-            new_instance = obj()
-            new_instance.id = data_item['id']
-            new_instance.name = data_item['name']
-            new_instance.localized_name = data_item['localized_name']
-            new_instance.save()
+
 
 
 def populate_heroes():
-    populate_instance(Hero, API_HEROES_URL, 'heroes')
+    heroes = populate_instance(Hero, API_HEROES_URL, 'heroes')
+
+    for hero in heroes:
+        try:
+            Hero.objects.get(pk=hero['id'])
+            continue
+        except Hero.DoesNotExist:
+            new_instance = Hero()
+            new_instance.id = hero['id']
+            new_instance.name = hero['name']
+            new_instance.localized_name = hero['localized_name']
+            new_instance.save()
+
 
 def populate_items():
-    populate_instance(Item, API_ITEMS_URL, 'items')
+    items = populate_instance(Item, API_ITEMS_URL, 'items')
+
+    for item in items:
+        try:
+            Item.objects.get(pk=item['id'])
+            continue
+        except Item.DoesNotExist:
+            new_instance = Item()
+            new_instance.id = item['id']
+            new_instance.name = item['name']
+            new_instance.localized_name = item['localized_name']
+            new_instance.is_recipe = item['recipe']
+            new_instance.cost = item['cost']
+            new_instance.save()
