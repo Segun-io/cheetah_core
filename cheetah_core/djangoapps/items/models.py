2afd035223af44f770835c9568c94d2ac9eb408e
santosa <angel_santos@outlook.com>
2019-04-21 17:28:10 -0500

    adding missing field to item model

--- cheetah_core/djangoapps/items/models.py
+++ cheetah_core/djangoapps/items/models.py
@@ -2,5 +2,16 @@ from django.db import models
 from core.models import ValveDataAbstract
 
 class Item(ValveDataAbstract):
+
+    CATEGORY_BUYABLE_ITEM = 'A'
+    CATEGORY_UNAVAILABLE_ITEM = 'B'
+
+    CATEGORY_CODES = (
+        (CATEGORY_BUYABLE_ITEM, 'Buyable'),
+        (CATEGORY_UNAVAILABLE_ITEM, 'Unavailable'),
+    )
+
     is_recipe = models.BooleanField('Is item recipe?', default=False)
     cost = models.PositiveIntegerField('Item cost', default=0)
+    category = models.CharField(
+        'Item category', max_length=1, choices=CATEGORY_CODES, default=CATEGORY_BUYABLE_ITEM)
