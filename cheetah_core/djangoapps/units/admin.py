9caf97852ace02d04259eb4528c3e6b66559ad7c
santosa <angel_santos@outlook.com>
2019-04-21 15:29:07 -0500

    typo in units

--- cheetah_core/djangoapps/units/admin.py
+++ cheetah_core/djangoapps/units/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 from .models import Unit
 
-@admin.register(Item)
-class ItemAdmin(admin.ModelAdmin):
+@admin.register(Unit)
+class UnitAdmin(admin.ModelAdmin):
     pass
